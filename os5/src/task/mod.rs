8edb6381ae0d7a035d4dd28904f91502c237ebd4
Yingkai Sun <yksuncs@gmail.com>
2022-07-31 10:48:35 +0800

    Pass test

--- os5/src/task/mod.rs
+++ os5/src/task/mod.rs
@@ -17,7 +17,7 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::loader::get_app_data_by_name;
+use crate::{config::BIG_STRIDE, loader::get_app_data_by_name};
 use alloc::sync::Arc;
 use lazy_static::*;
 use manager::fetch_task;
@@ -42,6 +42,7 @@ pub fn suspend_current_and_run_next() {
     let task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
     // Change status to Ready
     task_inner.task_status = TaskStatus::Ready;
+    task_inner.task_stride += BIG_STRIDE / task_inner.task_priority;
     drop(task_inner);
     // ---- release current PCB
 
